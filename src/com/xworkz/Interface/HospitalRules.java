0096ce2d2fe7878eab8eed743f76f12200b077fc
praveenkamatagi <72904455+praveenkamatagi@users.noreply.github.com>
2021-11-21 23:50:11 +0530

    Delete src/com/xworkz/Interface directory

--- src/com/xworkz/Interface/HospitalRules.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.Interface;
-
-public  interface HospitalRules {
-	public abstract String register();
-	public abstract double payAdvance();
-	public abstract boolean vaildInsurance();
-}
