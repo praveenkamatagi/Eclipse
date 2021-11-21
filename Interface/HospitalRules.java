dfe8843956f32dd7d2494793310cccef4eded87f
praveenkamatagi <72904455+praveenkamatagi@users.noreply.github.com>
2021-11-22 00:02:08 +0530

    interface

---
+++ Interface/HospitalRules.java
@@ -0,0 +1,7 @@
+package com.xworkz.Interface;
+
+public  interface HospitalRules {
+	public abstract String register();
+	public abstract double payAdvance();
+	public abstract boolean vaildInsurance();
+}
